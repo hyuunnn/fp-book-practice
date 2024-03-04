a7c6d418c5417e860804714a1097abcef4a32f00
Hyun Yi <hy00un__@naver.com>
2024-03-05 00:26:39 +0900

    Create 05.hs

---
+++ 05.hs
@@ -0,0 +1,20 @@
+-- fibo 6 => 8
+-- fibo 7 => 13
+fibo :: Int -> Int
+fibo 1 = 1
+fibo 2 = 1
+fibo n = fibo (n - 1) + fibo (n - 2)
+
+-- maxOfList [9, 2, 1] => 9
+-- maxOfList [4, 5, 2] => 5
+-- maxOfList [1, 2, 3] => 3
+maxOfList :: [Int] -> Int
+maxOfList [] = 0
+maxOfList [a] = a
+maxOfList (head:tail) = max head (maxOfList tail)
+
+-- nth [1, 2, 3] 2 => 2
+nth :: [Int] -> Int -> Int
+nth (head:tail) n
+  | n == 1    = head
+  | otherwise = nth tail (n - 1)
